cc660532ca6f934d800702b7d2f8ad516703d91a
Toan Nguyen <toannm11@viettel.com.vn>
2021-05-21 16:09:11 +0700

    add

--- db.js
+++ db.js
@@ -7,47 +7,64 @@ const sequelize = new Sequelize({
 });
 
 try {
-  await sequelize.authenticate();
+  sequelize.authenticate();
   console.log("Connection has been established successfully.");
 } catch (error) {
   console.error("Unable to connect to the database:", error);
 }
 
-const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.UUIDV4,
-        defaultValue: Sequelize.UUIDV4,
-        primaryKey: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    ethAddress: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    oneAddress: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    balance: {
-        type: DataTypes.DOUBLE,
-        defaultValue: 0.0
-    }
-})
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUIDV4,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  ethAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  oneAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  balance: {
+    type: DataTypes.DOUBLE,
+    defaultValue: 0.0,
+  },
+});
 
-await User.sync({ force: true });
+User.sync({});
 
 const createUser = (username, ethAddress, oneAddress, balance) => {
-  console.log('create user');
-  User.create({username: username, ethAddress: ethAddress, oneAddress, balance: balance});
-}
+  console.log("create user");
+  return User.create({
+    username: username,
+    ethAddress: ethAddress,
+    oneAddress,
+    balance: balance,
+  })
+    .then((u) => {
+      return u;
+    })
+    .catch((e) => {
+      console.log("create user error ", e);
+      throw e;
+    });
+};
 
-const findUser = async (username) => {
-  const u = await User.findOne({ where: { username: username}});
-  return u;
-}
+const findUser = function (username) {
+  return User.findOne({ where: { username: username } })
+    .then((rs) => {
+      return rs;
+    })
+    .catch((e) => {
+      console.log("findUse error ", e);
+      throw e;
+    });
+};
 
-// module.exports = createUser;
-export { createUser, findUser, User };
+export { createUser, findUser };
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { Messenger, HttpProvider } from "@harmony-js/network";
 import { ChainID, ChainType, Unit } from "@harmony-js/utils";
 import { TransactionFactory } from "@harmony-js/transaction";
 import { Harmony } from "@harmony-js/core";
-import { createUser, findUser, User } from './db.js';
+import { createUser, findUser, User } from "./db.js";
 const hmy = new Harmony("https://api.s0.b.hmny.io/", {
   chainType: ChainType.Harmony,
   chainId: ChainID.HmyTestnet,
@@ -15,26 +15,8 @@ const hmy = new Harmony("https://api.s0.b.hmny.io/", {
 const wallet = new Wallet(hmy);
 
 import config from "./credentials.js";
-// const creds = require("./credentials.json");
 
 const client = new Snoowrap(config);
-
-// Options object is a Snoowrap Listing object, but with subreddit and pollTime options
-// const comments = new CommentStream(client, {
-//   subreddit: "TestMyBotTip",
-//   limit: 10,
-//   pollTime: 2000,
-// });
-// comments.on("item", console.log);
-
-// const submissions = new SubmissionStream(client, {
-//   subreddit: "AskReddit",
-//   limit: 10,
-//   pollTime: 2000,
-// });
-// submissions.on("item", console.log);
-
-// const options = new InboxStreamOptions({filter: "mentio})
 
 const inbox = new InboxStream(client, {
   filter: "mentions" | "messages",
@@ -42,45 +24,70 @@ const inbox = new InboxStream(client, {
   pollTime: 2000,
 });
 
-async function sendMessage(to, subject, text){
-    await client.composeMessage({to: to, subject: subject, text: text});
+async function sendMessage(to, subject, text) {
+  await client.composeMessage({ to: to, subject: subject, text: text });
 }
 
 inbox.on("item", function (item) {
-    if (item.was_comment){
-        console.log('receive comment mention from ', item.author)
-        let c = client.getComment(item.parent_id);
-        c.author.then((a) => {
-            console.log("author ", a.name);
-        });
-        c.body.then((b) => {
-            console.log("body ", b);
-        });
+  if (item.new) {
+    if (item.was_comment) {
+      console.log("has new comment");
+      console.log("receive comment mention from ", item.author);
+      let c = client.getComment(item.parent_id);
+      // if (c.body.)
+      c.author.then((a) => {
+        console.log("author ", a.name);
+      });
+      c.body.then((b) => {
+        console.log("body ", b);
+      });
     } else {
-        console.log('receive private message from ', item.author)        
-        if (item.body === "create"){
-            if (findUser(item.author)){
-                console.log('user already existed');
+      console.log("has new message");
+      console.log("receive private message from ", item.author.name);
+      if (item.body === "create") {
+        // let u = await User.findOne({ where: { username: item.author } });
+        findUser(item.author.name)
+          .then((user) => {
+            if (user) {
+              console.log("user already existed");
             } else {
-                console.log('receive request create new address');
-                let mn = Wallet.generateMnemonic();
-                console.log('create mnemonic ', mn);
-                console.log('create account');
-                let account = hmy.wallet.addByMnemonic(mn);
-                console.log('account address ', account.bech32Address);
-                // client.composeMessage({
-                //     to: item.author,
-                //     subject: "Wallet Address",
-                //     text: "Here is your address " + account.bech32Address + " and eth version " + account.address
-                // }).then()
-                let text = "Here is your address " + account.bech32Address + " and eth version " + account.address;
-                sendMessage(item.author, "Wallet Address", text);
-                createUser(item.author, account.address, account.bech32Address, 0);
-                
+              console.log("receive request create new address");
+              let mn = Wallet.generateMnemonic();
+              console.log("create mnemonic ", mn);
+              console.log("create account");
+              let account = hmy.wallet.addByMnemonic(mn);
+              console.log("account address ", account.bech32Address);
+              createUser(
+                item.author.name,
+                account.address,
+                account.bech32Address,
+                0
+              )
+                .then((u) => {
+                  if (u) {
+                    let text =
+                      "Here is your address " +
+                      account.bech32Address +
+                      " and eth version " +
+                      account.address;
+                    sendMessage(item.author, "Wallet Address", text);
+                  }
+                })
+                .catch((err) => {
+                  console.log("create user error ", err);
+                });
             }
-        }
+          })
+          .catch((e) => {
+            console.log("find user error ", e);
+          });
+      }
     }
-    console.log('item ', item);
+    item.markAsRead().then((rs) => {
+      console.log(rs);
+    });
+  }
+  // console.log("item ", item);
 });
 
 // inbox.end();
