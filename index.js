0a0740d723afd0601c2f5a70a7cdd9644c031d22
Toan Nguyen <toanguyen@Toans-MacBook-Air.local>
2021-07-11 06:11:50 +0700

    ignore mention by botself

--- index.js
+++ index.js
@@ -121,6 +121,8 @@ async function processMention(item) {
             " body " +
             item.body
     );
+    if (item.author.name === botConfig.name)
+        return;
     let c = client.getComment(item.parent_id);
     let splitCms = item.body
         .toLowerCase()
